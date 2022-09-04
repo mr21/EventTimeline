293bf8cac41d76247b3191b4976425b5a291511e
mr21 <thomastortorini@gmail.com>
2022-09-04 23:24:18 +0200

    few first data

---
+++ data/events.js
@@ -0,0 +1,37 @@
+const EventsTimeline_events = {
+	moonLanding: {
+		name: "Moon landing",
+		date: "1969/07/16",
+		tags: "space,moon,coldwar",
+	},
+	coldwarStart: {
+		name: "Cold War start",
+		date: "1947/03/12",
+		tags: "coldwar",
+	},
+	coldwarEnd: {
+		name: "Cold War end",
+		date: "1991/12/26",
+		tags: "coldwar",
+	},
+	ww2Start: {
+		name: "World War 2",
+		date: "1939/09/01",
+		tags: "war,ww2",
+	},
+	ww2End: {
+		name: "World War 2 end",
+		date: "1945/09/02",
+		tags: "war,ww2",
+	},
+	ww1Start: {
+		name: "World War 1",
+		date: "1914/07/28",
+		tags: "war,ww1",
+	},
+	ww1End: {
+		name: "World War 1 end",
+		date: "1918/11/11",
+		tags: "war,ww1",
+	},
+};
---
+++ data/ranges.js
@@ -0,0 +1,17 @@
+const EventsTimeline_ranges = {
+	ww2: {
+		first: "ww2Start",
+		last: "ww2End",
+		tags: "war,ww2",
+	},
+	ww1: {
+		first: "ww1Start",
+		last: "ww1End",
+		tags: "war,ww1",
+	},
+	coldwar: {
+		first: "coldwarStart",
+		last: "coldwarEnd",
+		tags: "coldwar",
+	},
+};
