384c89dc1eb98c1e95ebd7912a72352d81fa495a
mr21 <thomastortorini@gmail.com>
2022-09-17 22:35:31 +0200

    append the events inside the ranges

--- data/events.js
+++ data/events.js
@@ -2,36 +2,36 @@ const EventsTimeline_events = {
 	moonLanding: {
 		name: "Moon landing",
 		date: "1969/07/16",
-		tags: "space,moon,coldwar",
+		ranges: "coldwar",
 	},
 	coldwarStart: {
-		name: "Cold War start",
+		name: "Cold War",
 		date: "1947/03/12",
-		tags: "coldwar",
+		ranges: "coldwar",
 	},
 	coldwarEnd: {
 		name: "Cold War end",
 		date: "1991/12/26",
-		tags: "coldwar",
+		ranges: "coldwar",
 	},
 	ww2Start: {
 		name: "World War 2",
 		date: "1939/09/01",
-		tags: "war,ww2",
+		ranges: "ww2",
 	},
 	ww2End: {
 		name: "World War 2 end",
 		date: "1945/09/02",
-		tags: "war,ww2",
+		ranges: "ww2",
 	},
 	ww1Start: {
 		name: "World War 1",
 		date: "1914/07/28",
-		tags: "war,ww1",
+		ranges: "ww1",
 	},
 	ww1End: {
 		name: "World War 1 end",
 		date: "1918/11/11",
-		tags: "war,ww1",
+		ranges: "ww1",
 	},
 };
--- data/ranges.js
+++ data/ranges.js
@@ -1,15 +1,18 @@
 const EventsTimeline_ranges = {
 	ww2: {
+		name: "World War 2",
 		first: "ww2Start",
 		last: "ww2End",
 		tags: "war,ww2",
 	},
 	ww1: {
+		name: "World War 1",
 		first: "ww1Start",
 		last: "ww1End",
 		tags: "war,ww1",
 	},
 	coldwar: {
+		name: "Cold War",
 		first: "coldwarStart",
 		last: "coldwarEnd",
 		tags: "coldwar",
